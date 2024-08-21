81e603f08492853e6f0a177227fb0297cef3c6a7
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-08-21 08:40:52 +0700

    top navbar

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,13 +9,27 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   static const List<Widget> _widgetOptions = <Widget>[
     MotorListView(),
     Text('Search Page'),
-    Text('Profile Page'),
+    Text('Profile Page')
   ];
 
   void _onItemTapped(int index) {
@@ -36,20 +50,27 @@ class _MainScreenState extends State<MainScreen> {
               fontWeight: FontWeight.bold,
             ),
           ),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'Recomended'),
+              Tab(text: 'Popular'),
+              Tab(text: 'News'),
+            ],
+          ),
         ),
-        body: SafeArea(
-          child: _widgetOptions.elementAt(_selectedIndex),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            MotorListView(),
+            MotorListView(),
+            MotorListView(),
+          ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
             BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
           ],
           currentIndex: _selectedIndex,
@@ -66,61 +87,55 @@ class MotorListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              final Motor motor = motorList[index];
-              return InkWell(
-                onTap: () {
-                  print('Card Cliked');
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(motor: motor),
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 100,
-                    child: Card(
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 1,
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset(motor.gambar),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [Text(motor.name), Text(motor.harga)],
-                            ),
-                          ),
-                        ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final Motor motor = motorList[index];
+        return InkWell(
+          onTap: () {
+            print('Card Cliked');
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailScreen(motor: motor),
+              ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 100,
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(motor.gambar),
+                        ),
                       ),
                     ),
-                  ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text(motor.name), Text(motor.harga)],
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
-            itemCount: motorList.length,
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
+      itemCount: motorList.length,
     );
   }
 }
--- lib/model/data_motor.dart
+++ lib/model/data_motor.dart
@@ -25,7 +25,7 @@ var motorList = [
       gambar: "assets/images/nomor_1.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "Z e-1",
@@ -35,7 +35,7 @@ var motorList = [
       gambar: "assets/images/nomor_2.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "KLX230 SE",
@@ -45,7 +45,7 @@ var motorList = [
       gambar: "assets/images/nomor_3.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "KLX230SM",
@@ -55,7 +55,7 @@ var motorList = [
       gambar: "assets/images/nomor_4.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "KLX230SM SE",
@@ -65,7 +65,7 @@ var motorList = [
       gambar: "assets/images/nomor_5.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "Ninja ZX-10R ABS KRT EDITION",
@@ -75,7 +75,7 @@ var motorList = [
       gambar: "assets/images/nomor_6.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "NINJA ZX-6R KRT EDITION",
@@ -85,7 +85,7 @@ var motorList = [
       gambar: "assets/images/nomor_7.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "NINJA ZX-4RR",
@@ -95,7 +95,7 @@ var motorList = [
       gambar: "assets/images/nomor_8.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "W175 SE",
@@ -105,7 +105,7 @@ var motorList = [
       gambar: "assets/images/nomor_9.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
   Motor(
       name: "W175 CAFE",
@@ -115,6 +115,6 @@ var motorList = [
       gambar: "assets/images/nomor_10.jpg",
       rating: "4.5",
       urlGambar: [
-        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFxOkiikiEvLMPDp0fP1IOB2qP2GippWIZPw&s'
       ]),
 ];
